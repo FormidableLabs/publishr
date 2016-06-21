73a20f150aa79f19055072a3f7820ab7833f38df
Jeff McRiffey <jmcriffey@gmail.com>
2016-06-21 15:56:55 -0400

    fix git clean command

--- src/postpublish.js
+++ src/postpublish.js
@@ -2,7 +2,7 @@ import {exec} from 'child_process';
 
 
 export default function postpublish() {
-  exec('git clean -d && git checkout .', (err, stdout) => {
+  exec('git clean -df && git checkout .', (err, stdout) => {
     if (err) {
       console.log(err);
     }
